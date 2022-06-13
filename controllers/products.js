ceb701b27385e5033893ed98783ae9785b3ba171
tosinibrahim96 <tosinibrahim96@gmail.com>
2022-06-13 10:25:04 +0100

    query object refactor

--- controllers/products.js
+++ controllers/products.js
@@ -6,7 +6,34 @@ const getAllProductsStatic = async (req, res) => {
 };
 
 const getAllProducts = async (req, res) => {
-  const products = await Product.find(req.query);
+  const { featured, company, name, sort } = req.query;
+  const queryObject = {};
+
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+  }
+
+  if (company) {
+    queryObject.company = company;
+  }
+
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
+  }
+
+  let result = Product.find(queryObject);
+
+  if (sort) {
+    /**
+     * change all the comma to space
+     */
+    const sortList = sort.split(",").join(" ");
+    result = result.sort(sortList);
+  } else {
+    result = result.sort("createdAt");
+  }
+
+  const products = await result;
   res.status(200).json({ products });
 };
 
