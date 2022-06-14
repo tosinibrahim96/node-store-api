885c289f861639b80fb31fc37f50592a501655e2
tosinibrahim96 <tosinibrahim96@gmail.com>
2022-06-14 09:07:18 +0100

    Pagination complete

--- controllers/products.js
+++ controllers/products.js
@@ -6,7 +6,7 @@ const getAllProductsStatic = async (req, res) => {
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort } = req.query;
+  const { featured, company, name, sort, fields } = req.query;
   const queryObject = {};
 
   if (featured) {
@@ -32,6 +32,20 @@ const getAllProducts = async (req, res) => {
   } else {
     result = result.sort("createdAt");
   }
+
+  if (fields) {
+    /**
+     * change all the comma to space
+     */
+    const fieldsList = fields.split(",").join(" ");
+    result = result.select(fieldsList);
+  }
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
 
   const products = await result;
   res.status(200).json({ products });
