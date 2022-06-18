4a1b9c47c31fe3c557560fbdba652e67e8522f1c
tosinibrahim96 <tosinibrahim96@gmail.com>
2022-06-18 10:25:30 +0100

    Numeric filters done

--- controllers/products.js
+++ controllers/products.js
@@ -6,7 +6,7 @@ const getAllProductsStatic = async (req, res) => {
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort, fields } = req.query;
+  const { featured, company, name, sort, fields, numericFilters } = req.query;
   const queryObject = {};
 
   if (featured) {
@@ -19,6 +19,32 @@ const getAllProducts = async (req, res) => {
 
   if (name) {
     queryObject.name = { $regex: name, $options: "i" };
+  }
+
+  if (numericFilters) {
+    const operatorMap = {
+      ">": "$gt",
+      "<": "$lt",
+      "=": "$eq",
+      ">=": "$gte",
+      "<=": "$gte",
+    };
+
+    const regex = /\b(<|>|=|<=|>=)\b/g;
+    let filters = numericFilters.replace(
+      regex,
+      (match) => `-${operatorMap[match]}-`
+    );
+
+    const options = ["price", "rating"];
+    filters = filters.split(",").forEach((item) => {
+      const [field, operator, value] = item.split("-");
+      if (options.includes(field)) {
+        queryObject[field] = { [operator]: Number(value) };
+      }
+    });
+
+    console.log({ numericFilters });
   }
 
   let result = Product.find(queryObject);
